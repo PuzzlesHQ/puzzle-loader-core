259bb2f7550866a699a27d8d4ac87908104c42ef
Mr_Zombii <thezombiigames@gmail.com>
2025-12-13 00:11:51 -0600

    fix proxy class name format

--- src/common/java/dev/puzzleshq/puzzleloader/loader/util/proxy/ProxyClassUtil.java
+++ src/common/java/dev/puzzleshq/puzzleloader/loader/util/proxy/ProxyClassUtil.java
@@ -17,7 +17,7 @@ public class ProxyClassUtil {
         ClassNode node = getNode(clazz);
         String[] exceptions = getExceptions(node).toArray(new String[0]);
 
-        String proxyClassName = "dev.puzzleshq.loader.proxyclass.GameLoaderProxy";
+        String proxyClassName = "dev/puzzleshq/loader/proxyclass/GameLoaderProxy";
 
         ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
         ProxyClassWriter proxyClassWriter = new ProxyClassWriter(writer, isInterface(node), clazz, exceptions);
